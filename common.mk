2daff2864abf84ff2f0dc6ad7031e11dcadc980a
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-08 21:33:49 -0400

    hwaddrs: Move bluetooth addr to data/misc/bluetooth
    SELinux for /data/misc/bdaddr is a pain, so just put it in
    /data/misc/bluetooth/bdaddr. Also change permissions on
    /data/misc/bluetooth/bdaddr on hwaddr service stopped.
    
    Change-Id: Ie669d41d7e9e504faaa3b3a438266b66267b3783

--- common.mk
+++ common.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,4 +21,4 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bt.bdaddr_path="/data/misc/bdaddr"
+    ro.bt.bdaddr_path="/data/misc/bluetooth/bdaddr"
