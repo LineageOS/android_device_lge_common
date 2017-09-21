c98037a76997d7fa8bc0e143ca6ee65216be4465
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-21 07:59:58 -0700

    hwaddrs: Move hwaddrs to vendor
    Change-Id: I6fabdf0ed55c23acd0ee5b3607e6d2a731a343be

--- hwaddrs/Android.mk
+++ hwaddrs/Android.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2011-2015 The CyanogenMod project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,5 +24,7 @@ LOCAL_SRC_FILES := getmac.c
 LOCAL_PRELINK_MODULE := false
 
 LOCAL_MODULE := hwaddrs
+
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_EXECUTABLE)
