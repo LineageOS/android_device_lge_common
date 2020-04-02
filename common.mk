fb2781175d5c839c6a3a1a323c3cbe02011e7a1b
Elliott Mitchell <ehem+lineageos@m5p.com>
2020-04-01 23:45:40 -0700

    hwaddrs: Setup for build-time configuration
    Moving to the common area means these need to be modifiable to match the
    device type.  As such create Make variables to allow them to be modified
    when built.
    
    Change-Id: I92e67894786d6b8124c9249372dc521efc4e1a59

--- common.mk
+++ common.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017,2020 The LineageOS Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,9 +17,5 @@
 
 # WiFi/Bluetooth MAC addresses
 PRODUCT_PACKAGES += \
-    hwaddrs \
+    hwaddrs2 \
     hwaddrs.readmisc
-
-# Bluetooth
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bt.bdaddr_path="/data/misc/bluetooth/bdaddr"
