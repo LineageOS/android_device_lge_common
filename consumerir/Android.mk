e9666585c7fbc10179913990eb7eef1be394e415
Elliott Mitchell <ehem+android_kernel_lge_msm8996@m5p.com>
2020-05-08 19:04:24 -0700

    lge: Add enable/disable for IR HAL
    Not all LGE devices include IR hardware.  As common should cater to all
    LGE devices, make it possible to disable.
    
    This defaults to enabled due to history of LGE platforms.  The devices
    which have been actively using common assume the default is enabled.
    This SHOULD change in the future!
    
    Change-Id: I0332105689b0f6cc93a75c7966fda23117eef028

--- consumerir/Android.mk
+++ consumerir/Android.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifneq ($(TARGET_ENABLE_COMMON_IR),false)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -42,3 +44,4 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 
 include $(BUILD_EXECUTABLE)
 
+endif
